5626e0b877ad5cb719f9e0b9c3941f8abf3fc15a
AnnaPCH <73597469+AnnaPCH@users.noreply.github.com>
2021-01-25 23:10:25 +0200

    styling css

--- src/Weather.js
+++ src/Weather.js
@@ -4,15 +4,17 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <br />
       <h1> 19 </h1>
       <span className="links">
 <a href="/"> °C  </a> | <a href="/"> °F</a>
       </span>
       <img
         src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-        alt="#"
+        alt="partly cloudy"
       />
       <div className="description"> Partly cloudy </div>
+      <hr />
     </div>
   );
 }
